cc8cdedf616a997048da70001fbce888284365f5
Andrew McClure <andrew@southweb.co.nz>
2014-10-12 09:04:07 +1300

    Removed crud

--- topicmanagers/digisense/__init__.py
+++ topicmanagers/digisense/__init__.py
@@ -4,9 +4,5 @@ import os
 
 def classFactory():
     from dstopicmanager import dsTopicManager
-    print __file__
- #   print os.path.dirname(__file__)
-    dstopicmanager.deviceTypesPath = os.path.join(os.path.dirname(__file__),"dsdevices.xml")
-#    print dstopicmanager.deviceTypesPath
     return {"name":"DigiSense","class":dsTopicManager}
 
